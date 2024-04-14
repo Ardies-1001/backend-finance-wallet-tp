08e4d9d092f451dbf27f1bb0babfd1fad1214149
Ardies Lebon <81700296+Ardies-1001@users.noreply.github.com>
2024-04-14 10:21:49 +0100

    Remise de la config mysql en local

--- env.ts
+++ env.ts
@@ -1,7 +1,7 @@
 /*
-|-------------------------------------------------------------------------
+|--------------------------------------------------------------------------
 | Validating Environment Variables
-|-------------------------------------------------------------------------
+|--------------------------------------------------------------------------
 |
 | In this file we define the rules for validating environment variables.
 | By performing validation we ensure that your application is running in
@@ -24,9 +24,15 @@ export default Env.rules({
     DRIVE_DISK: Env.schema.enum(['local'] as const),
 	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
 	
-	PG_HOST: Env.schema.string({ format: 'host' }),
-    PG_PORT: Env.schema.number(),
-    PG_USER: Env.schema.string(),
-    PG_PASSWORD: Env.schema.string.optional(),
-    PG_DB_NAME: Env.schema.string(),
+	// PG_HOST: Env.schema.string({ format: 'host' }),
+    // PG_PORT: Env.schema.number(),
+    // PG_USER: Env.schema.string(),
+    // PG_PASSWORD: Env.schema.string.optional(),
+    // PG_DB_NAME: Env.schema.string(),
+
+	MYSQL_HOST: Env.schema.string({ format: 'host' }),
+    MYSQL_PORT: Env.schema.number(),
+    MYSQL_USER: Env.schema.string(),
+    MYSQL_PASSWORD: Env.schema.string.optional(),
+    MYSQL_DB_NAME: Env.schema.string(),
 })
